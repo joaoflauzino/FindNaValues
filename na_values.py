76ba52486710c8507b96c38798c43ebfe2421d04
root <root@DESKTOP-V0EV4NI.localdomain>
2020-08-19 09:10:12 -0300

    Fix

--- na_values.py
+++ na_values.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import numpy as np
+import math
 
 df = pd.read_csv('CC GENERAL.csv', sep = ",", na_values = ['?', '', 'NA'])
-columns = df.columns 
-types_na = {"float64": lambda x: df[x].mean() if np.isnan(x) else x, 
-         "int64": lambda x: int(df[x].mean()) if np.isnan(x) else x, 
-         "object": lambda x: df[x].mode() if str(x) == 'nan' else x}
+
+columns = df.columns
+
+types_na = {"float64": lambda x: df[x].mean() if math.isnan(x) == True else x, 
+         "int64": lambda x: int(df[x].mean()) if math.isnan(x) == True else x,
+         "object": lambda x: df[x].mode() if str(x) == None else x}
 
 def check_na_values():
-    for i, v in enumerate(columns):
-        df[v] = df[v].apply(types_na[str(df[v].dtype)])
-
+    for i in columns:
+        df[i] = df[i].apply(types_na[str(df[i].dtype)])
+    
     print(df.isnull().sum())
 
 if __name__ == "__main__":
 
     check_na_values()
 
-    
-                                       
